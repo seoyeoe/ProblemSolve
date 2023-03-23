36a8363f67bf8f7bda7975c3c293d178efe8ffee
seoyeone <seoyeonev@gmail.com>
2023-03-23 19:16:43 +0900

    quiz1

---
+++ quiz3.java
@@ -0,0 +1,12 @@
+package week1;
+
+public class quiz3 {
+	public static void main(String[] args) {
+		int num1 = 0;
+		int num2 = 0;
+		int answer = 0;
+
+		answer = num1 + num2;
+		System.out.println(answer);
+	}
+}
