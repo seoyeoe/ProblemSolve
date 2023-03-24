00a1102c3049f55f85957af6987a344cf7392f88
seoyeoe <122018643+seoyeoe@users.noreply.github.com>
2023-03-24 09:22:56 +0900

    Add files via upload
    [1주차 1번째 두수의곱] 이서연

---
+++ LSY/두수의곱.java
@@ -0,0 +1,14 @@
+{\rtf1\ansi\ansicpg949\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 class Solution \{\
+    public int solution(int num1, int num2) \{\
+        int answer = 0;\
+        answer= num1-num2;\
+        return answer;\
+    \}\
+\}}
